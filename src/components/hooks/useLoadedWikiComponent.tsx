77592d2dd2ccc0d6e581aebd1de15cfc476e4b69
Willian Pinheiro <willpinhaaa@gmail.com>
2025-01-24 17:00:41 -0300

    remove await wait

--- src/components/hooks/useLoadedWikiComponent.tsx
+++ src/components/hooks/useLoadedWikiComponent.tsx
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { wait } from "../../lib/wait";
 import { loadWikiComponent, WikiComponent } from "../../lib/wiki";
 
 export function useLoadedWikiComponent(component: WikiComponent) {
 	return useQuery({
 		queryKey: ["wiki-component", component],
 		queryFn: async () => {
-			await wait(500); // Simulate loading time
-
 			return await loadWikiComponent(component);
 		},
 	});
