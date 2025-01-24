f368fff095d1573c22d4dadc97c69963c27c395f
Willian Pinheiro <willpinhaaa@gmail.com>
2025-01-24 20:15:39 -0300

    add more alerts

---
+++ src/wiki/20250124231308/Index.tsx
@@ -0,0 +1,9 @@
+import { Alert } from "@mantine/core";
+
+export function Index() {
+	return (
+		<Alert title="Outline alert" variant="outline">
+			This is an outline alert
+		</Alert>
+	);
+}
---
+++ src/wiki/20250124231446/Index.tsx
@@ -0,0 +1,9 @@
+import { Alert } from "@mantine/core";
+
+export function Index() {
+	return (
+		<Alert title="Filled alert" variant="filled">
+			This is a filled alert
+		</Alert>
+	);
+}
