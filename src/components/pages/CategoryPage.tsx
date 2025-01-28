a8cc395b7e680dbcd8558f74325ce661bd410fed
Willian Pinheiro <willpinhaaa@gmail.com>
2025-01-28 12:58:29 -0300

    add container to category page

--- src/components/pages/CategoryPage.tsx
+++ src/components/pages/CategoryPage.tsx
@@ -1,4 +1,12 @@
-import { Button, Group, Stack, Text, ThemeIcon, Title } from "@mantine/core";
+import {
+	Button,
+	Container,
+	Group,
+	Stack,
+	Text,
+	ThemeIcon,
+	Title,
+} from "@mantine/core";
 import { IconArrowLeft, IconBrandReact } from "@tabler/icons-react";
 import { Link, useParams } from "react-router";
 import { wiki } from "../../lib/wiki";
@@ -14,40 +22,46 @@ export function CategoryPage() {
 	const numComponents = Object.keys(wiki[category]).length;
 
 	return (
-		<Stack gap="xl" p="lg">
-			<Group justify="space-between">
-				<Button
-					variant="subtle"
-					color="gray"
-					leftSection={<IconArrowLeft />}
-					component={Link}
-					to="/"
-				>
-					Go back to categories
-				</Button>
+		<Container size={1440} p="lg">
+			<Stack gap="xl">
+				<Group justify="space-between">
+					<Button
+						variant="subtle"
+						color="gray"
+						leftSection={<IconArrowLeft />}
+						component={Link}
+						to="/"
+					>
+						Go back to categories
+					</Button>
 
-				<Group gap="xs">
-					<ThemeIcon color="#04D0F4" size="xs" variant="transparent">
-						<IconBrandReact />
-					</ThemeIcon>
-					<Text fw="bold" size="sm">
-						{numComponents} components
-					</Text>
+					<Group gap="xs">
+						<ThemeIcon
+							color="#04D0F4"
+							size="xs"
+							variant="transparent"
+						>
+							<IconBrandReact />
+						</ThemeIcon>
+						<Text fw="bold" size="sm">
+							{numComponents} components
+						</Text>
+					</Group>
 				</Group>
-			</Group>
 
-			<Title>{category}</Title>
+				<Title>{category}</Title>
 
-			{Object.entries(wiki[category]).map(
-				([componentName, component]) => (
-					<WikiComponentView
-						key={componentName}
-						category={category}
-						componentName={componentName}
-						component={component}
-					/>
-				)
-			)}
-		</Stack>
+				{Object.entries(wiki[category]).map(
+					([componentName, component]) => (
+						<WikiComponentView
+							key={componentName}
+							category={category}
+							componentName={componentName}
+							component={component}
+						/>
+					)
+				)}
+			</Stack>
+		</Container>
 	);
 }
