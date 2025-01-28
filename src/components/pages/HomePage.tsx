98de3b17e8fdfe69835fc8c2c7081895bf428a9b
Willian Pinheiro <willpinhaaa@gmail.com>
2025-01-28 15:38:19 -0300

    change logo

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,3 +1,53 @@
+import {
+	ActionIcon,
+	Group,
+	Image,
+	Stack,
+	Text,
+	Title,
+	Tooltip,
+} from "@mantine/core";
+import { IconBrandGithub, IconHeartFilled } from "@tabler/icons-react";
+import { Link } from "react-router";
+import { ThemeButton } from "../button/ThemeButton";
+
 export function HomePage() {
-	return "";
+	return (
+		<Stack gap="xl" p="md" align="center">
+			<Stack align="center" gap="xs">
+				<Image src="/images/logo.svg" w={180} />
+
+				<Title size={40}>
+					react.
+					<Text
+						span
+						inherit
+						c="blue"
+						variant="gradient"
+						gradient={{ from: "#04D0F4", to: "blue" }}
+					>
+						computer
+					</Text>
+				</Title>
+
+				<Group gap="xs">
+					<Tooltip label="Sponsor">
+						<ActionIcon color="red">
+							<IconHeartFilled />
+						</ActionIcon>
+					</Tooltip>
+					<Tooltip label="GitHub">
+						<ActionIcon
+							variant="default"
+							component={Link}
+							to="https://github.com/willpinha/react-computer"
+						>
+							<IconBrandGithub />
+						</ActionIcon>
+					</Tooltip>
+					<ThemeButton />
+				</Group>
+			</Stack>
+		</Stack>
+	);
 }
