817bf1cad1e7757d5ea55e4c4012463c8c495307
Willian Pinheiro <willpinhaaa@gmail.com>
2025-01-29 14:23:46 -0300

    add tooltip

---
+++ src/wiki/tooltip/Simple/Index.tsx
@@ -0,0 +1,9 @@
+import { Button, Tooltip } from "@mantine/core";
+
+export function Index() {
+	return (
+		<Tooltip label="Just a simple tooltip">
+			<Button>Hover me</Button>
+		</Tooltip>
+	);
+}
