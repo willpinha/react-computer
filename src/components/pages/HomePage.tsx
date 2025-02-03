bab55440beba8d6ab7cc8ab4882b6f24e67d4c58
Willian Pinheiro <willpinhaaa@gmail.com>
2025-02-03 10:38:54 -0300

    add mock of search categories input in home page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -5,8 +5,11 @@ import {
 	Container,
 	Group,
 	Image,
+	SimpleGrid,
 	Stack,
 	Text,
+	TextInput,
+	ThemeIcon,
 	Title,
 	Tooltip,
 } from "@mantine/core";
@@ -59,34 +62,21 @@ function SearchCategoriesButton() {
 export function HomePage() {
 	return (
 		<Stack gap="xl" p="md" align="center">
-			<Stack align="center" gap="md">
+			<Stack gap="xs" align="center">
 				<Image src="/images/logo.svg" w={180} />
 
-				<Stack gap={0} align="center">
-					<Title size={40}>
-						react.
-						<Text
-							span
-							inherit
-							c="blue"
-							variant="gradient"
-							gradient={{ from: "#04D0F4", to: "blue" }}
-						>
-							computer
-						</Text>
-					</Title>
-
-					<Text ta="center">
-						<Badge variant="outline" size="xs" component="span">
-							{numComponents()}
-						</Badge>{" "}
-						components in{" "}
-						<Badge variant="outline" size="xs" component="span">
-							{numCategories()}
-						</Badge>{" "}
-						categories
+				<Title size={40}>
+					react.
+					<Text
+						span
+						inherit
+						c="blue"
+						variant="gradient"
+						gradient={{ from: "#04D0F4", to: "blue" }}
+					>
+						computer
 					</Text>
-				</Stack>
+				</Title>
 
 				<Group gap="xs">
 					<Tooltip label="Sponsor" withArrow>
@@ -124,31 +114,58 @@ export function HomePage() {
 							<IconBrandGithub />
 						</ActionIcon>
 					</Tooltip>
-					<SearchCategoriesButton />
 					<ThemeButton />
 				</Group>
 			</Stack>
 
-			<Container size="lg">
-				<Group justify="center">
+			<Stack gap="xs" w="100%" align="center">
+				<TextInput
+					w="100%"
+					maw={400}
+					placeholder="Search categories"
+					leftSection={<IconSearch size={20} />}
+				/>
+
+				<Text ta="center">
+					<Badge variant="outline" size="xs" component="span">
+						{numComponents()}
+					</Badge>{" "}
+					components in{" "}
+					<Badge variant="outline" size="xs" component="span">
+						{numCategories()}
+					</Badge>{" "}
+					categories
+				</Text>
+			</Stack>
+
+			<Container w="100%" size="lg">
+				<SimpleGrid cols={{ xs: 1, sm: 2, md: 5 }} w="100%">
 					{Object.entries(wiki).map(([category, components]) => (
 						<Button
 							key={category}
-							leftSection={<IconHash size={20} />}
+							justify="space-between"
+							leftSection={
+								<ThemeIcon
+									variant="transparent"
+									color="blue"
+									size="xs"
+								>
+									<IconHash />
+								</ThemeIcon>
+							}
 							rightSection={
 								<Text inherit c="dimmed" size="xs">
 									{Object.keys(components).length}
 								</Text>
 							}
 							variant="default"
-							radius="lg"
 							component={Link}
 							to={`/categories/${category}`}
 						>
 							{category}
 						</Button>
 					))}
-				</Group>
+				</SimpleGrid>
 			</Container>
 		</Stack>
 	);
